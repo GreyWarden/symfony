bb5e58214bb45db934286cc3d63ad03390214be2
Raúl Pavón <raulpavonfp@gmail.com>
2020-11-04 18:20:02 +0100

    Añado sesiones

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Service\GiftService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     private GiftService $giftService;
+    private SessionInterface $session;
+    private LoggerInterface $logger;
 
-    public function __construct(GiftService $giftService)
+    public function __construct(GiftService $giftService, SessionInterface $session, LoggerInterface $logger)
     {
         $this->giftService = $giftService;
+        $this->session = $session;
+        $this->logger = $logger;
     }
 
     /**
@@ -25,6 +31,11 @@ class DefaultController extends AbstractController
 
         $this->addFlash('notice', 'Cosas hechas!');
         $this->addFlash('warning', 'Cosas no hechas!');
+        $this->session->set('name', 'hello there');
+
+        if ($this->session->has('name')) {
+            $this->logger->info($this->session->get('name'));
+        }
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'General Controlly',
