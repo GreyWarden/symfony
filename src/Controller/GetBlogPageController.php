117e7b8a35932547f1d07d142a500372f0cc2c01
Raúl Pavón <raulpavonfp@gmail.com>
2020-11-04 18:14:38 +0100

    Añado cookie

--- src/Controller/GetBlogPageController.php
+++ src/Controller/GetBlogPageController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,16 @@ final class GetBlogPageController extends AbstractController
     /**
      * @Route("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
      */
-    public function getBlogPage()
+    public function getBlogPage(): Response
     {
-        return new Response('Hello there');
+        $cookie = new Cookie(
+            'a_cookie',
+            'hello there!',
+            time() + (2 * 365 * 24 * 3600)
+        );
+
+        $response = new Response('Hello there');
+        $response->headers->setCookie($cookie);
+        return $response;
     }
 }
