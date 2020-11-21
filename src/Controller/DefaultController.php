ecede1c78c429fadc9591d8ea8c12fe764dd0af6
Raúl Pavón <raulpavonfp@gmail.com>
2020-11-21 12:06:07 +0100

    Ejemplo de cache

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,16 +7,19 @@ use App\Services\GiftService;
 use App\Services\VideoUploaderServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class DefaultController extends AbstractController
 {
     private GiftService $giftService;
     private VideoUploaderServiceInterface $videoUploaderService;
+    private FilesystemAdapter $cache;
 
     public function __construct(GiftService $giftService, VideoUploaderServiceInterface $videoUploaderService)
     {
         $this->giftService = $giftService;
         $this->videoUploaderService = $videoUploaderService;
+        $this->cache = new FilesystemAdapter();
     }
 
     /**
@@ -25,6 +28,14 @@ class DefaultController extends AbstractController
     public function index()
     {
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        $posts = $this->cache->getItem('database.get_posts');
+        if(!$posts->isHit()) {
+            $postsFromDB = ['post1', 'post2', 'post3'];
+            $posts->set(serialize($postsFromDB));
+            $posts->expiresAfter(5);
+            $this->cache->save($posts);
+        }
+        dump(unserialize($posts->get()));
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'General Controlly',
